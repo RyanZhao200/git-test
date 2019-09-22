40733804be305228ad44e9f6d40c69fcf5f9c684
Ryan <ryanzhao200@foxmail.com>
2019-09-22 17:47:04 +0800

    将本地工程添加到git

---
+++ git-project-test/src/com/itheima/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello git...");
+        System.out.println("Hello git...");
+        System.out.println("Hello git...");
+        System.out.println("Hello git...");
+    }
+}
